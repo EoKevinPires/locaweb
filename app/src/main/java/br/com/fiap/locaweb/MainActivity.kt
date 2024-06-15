f0506d7e7a9882177083285390778e057bbcdf92
Kevin <kevinpiresdeoliveira@gmail.com>
2024-06-15 19:33:11 -0300

    activity_login: tela para login;

--- app/src/main/java/br/com/fiap/locaweb/MainActivity.kt
+++ app/src/main/java/br/com/fiap/locaweb/MainActivity.kt
@@ -6,6 +6,8 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro)
+        setContentView(R.layout.activity_login)
+
+
     }
 }
